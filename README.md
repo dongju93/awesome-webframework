bb98516fd7541aaa58a157e3ec93969fed888350
dong-ju <spdlqj011@gmail.com>
2023-10-15 16:58:37 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+##### RUN
+```
+cargo watch -q -c -w src/ -x run
+```
+
+##### TEST
+```
+cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
+```
+
+##### Course
+    https://youtu.be/XZtlD_m59sM?si=vm1edfOE5rDxDVnb
+
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!</strong>") }),
+    );
+
+    // start server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("-->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
+    // end server
+}
